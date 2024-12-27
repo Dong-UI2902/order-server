bf42fcdd93ff747676d4e31aefde0d714b935ab0
Dong-UI2902 <dongnv2902@gmail.com>
2024-12-27 17:53:51 +0700

    update

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const server = http.createServer(app);
 const cors = require("cors");
 app.use(
   cors({
-    origin: [
-      "http://127.0.0.1:3000/",
-      "http://localhost:3000",
-      "https://inspiring-jelly-43332e.netlify.app",
-    ],
+    origin: ["*"],
     credentials: true,
     methods: ["POST", "PUT", "GET", "DELETE"],
   })
